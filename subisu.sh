3450491ee7837e6262cfe0bb4bf74a1d95036ab0
blacpythoz <blacpythoz@gmail.com>
2017-03-30 22:45:31 +0545

    Some changes

--- subisu.sh
+++ subisu.sh
@@ -11,6 +11,7 @@ monthAgo=`date +%Y-%m-%d -d "${today} day ago"`
 
 
 ## Get Input
+getInput() {
 echo -n "User name: "
 read username
 echo -n "Password: "
@@ -18,26 +19,29 @@ read password
 echo -n "From Date(yyyy-mm-dd) "
 echo "[Default: yesterday] "
 read fromDate
-
     if [[ -z "$fromDate" ]]; then
         fromDate=`date +%Y-%m-%d -d "yesterday"`
     fi
-
 echo -n "To Date (yyyy-mm-dd) : "
 echo "[Default: Today]  "
 read toDate
-
     if [[ -z "$toDate" ]]; then
         toDate=`date +%Y-%m-%d`
     fi
+}
 
-
-curl -s --cookie-jar /tmp/subisu "http://myaccount.subisu.net.np/userportal/newlogin.do?phone=0" --data "type=1&username=${username}&password=${password}" > debugLog
-checkInvalid=`grep "Access Denied" debugLog`
+getAccess() {
+    curl -s --cookie-jar /tmp/subisu "http://myaccount.subisu.net.np/userportal/newlogin.do?phone=0" --data "type=1&username=${username}&password=${password}" > debugLog
+    checkInvalid=`grep "Access Denied" debugLog`
     if [[ -n $checkInvalid ]]
     then
         echo "Access Denied. Username/password is incorrect"
-    else
+        exit 1
+    fi
+}
+
+
+getInfo() {
         curl -s --cookie /tmp/subisu "http://myaccount.subisu.net.np/userportal/usageSummary.do" --data "pageTitle=Usage+Summary&fromdate=${fromDate}&todate=${toDate}&actid=${username}&period=last12hrs&submit=Go" --compressed > debug
     IFS=$'\n'
 
@@ -53,4 +57,8 @@ checkInvalid=`grep "Access Denied" debugLog`
     echo "Download: ${infos[4]}"
     echo "Total: ${infos[5]}"
     echo -e "\n\n"
-fi
+}
+
+getInput
+getAccess
+getInfo
